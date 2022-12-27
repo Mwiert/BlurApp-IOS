38a2fc7abd69efc101b32dc9208d8716935e954a
MwiertAir <mertsahin011@icloud.com>
2022-12-27 06:31:18 +0300

    Initial commit

---
+++ BlurApp-IOSUITests/BlurApp_IOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BlurApp_IOSUITestsLaunchTests.swift
+//  BlurApp-IOSUITests
+//
+//  Created by Mert Şahin on 27.12.2022.
+//
+
+import XCTest
+
+final class BlurApp_IOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
